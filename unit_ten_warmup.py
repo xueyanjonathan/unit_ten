371748c2aba35ec76fab19f2ed6a978f92a0ec59
Jonathan <jonathan@iMac9.local>
2018-12-03 11:35:53 -0500

    Setting up a Pygame window

---
+++ unit_ten_warmup.py
@@ -0,0 +1,13 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+mainSurface = pygame.display.set_mode((500, 500), 0, 32)
+pygame.display.set_capition("Sandy Spring Friends School")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
