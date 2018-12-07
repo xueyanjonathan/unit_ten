ec35dd5bf17fe16fb09a8180734e58dfcb8a09fb
Jonathan <jonathan@iMac9.local>
2018-12-07 08:17:21 -0500

    Created trellis for the logo

--- logo.py
+++ logo.py
@@ -13,3 +13,10 @@ class Logo:
         pygame.draw.rect(self.main_surface, self.GREEN, (5, 5, 400, 200), 3)
         pygame.draw.rect(self.main_surface, self.GREEN, (10, 10, 390, 190), 0)
         pygame.display.update()
+
+    def draw_trellis(self):
+        pygame.draw.polygon(self.main_surface, self.GOLD, ((145, 100), (265, 100), (205, 20)), 3)
+        pygame.draw.line(self.main_surface, self.GOLD, (205, 100), (205, 20), 3)
+        pygame.draw.line(self.main_surface, self.GOLD, (205, 100), (235, 60), 3)
+        pygame.draw.line(self.main_surface, self.GOLD, (205, 100), (175, 60), 3)
+        pygame.display.update()
--- unit_ten_warmup.py
+++ unit_ten_warmup.py
@@ -8,6 +8,7 @@ mainSurface = pygame.display.set_mode((500, 250), 0, 32)
 pygame.display.set_caption("Sandy Spring Friends School")
 ssfs = logo.Logo(mainSurface)
 ssfs.draw_rectangles()
+ssfs.draw_trellis()
 
 while True:
     for event in pygame.event.get():
